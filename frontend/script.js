6627e770961530dcaa055dc09af0617f020ce7e5
afzalafzql11-max <afzalafzql11@gmail.com>
2026-03-11 19:42:13 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,76 @@
 const API = "https://image1dehazer.onrender.com"
 
+let USER_ID = null
+
+
+async function signup(){
+
+const email = document.getElementById("signup_email").value
+const password = document.getElementById("signup_password").value
+
+const res = await fetch(API + "/signup",{
+
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({email,password})
+
+})
+
+const data = await res.json()
+
+alert(data.status)
+
+}
+
+
+
+async function login(){
+
+const email = document.getElementById("login_email").value
+const password = document.getElementById("login_password").value
+
+const res = await fetch(API + "/login",{
+
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({email,password})
+
+})
+
+const data = await res.json()
+
+if(data.status==="success"){
+
+USER_ID = data.user_id
+
+document.getElementById("auth").style.display="none"
+document.getElementById("dashboard").style.display="block"
+
+}else{
+
+alert("Invalid login")
+
+}
+
+}
+
+
+
 async function uploadImage(){
 
 const file = document.getElementById("image").files[0]
 
 if(!file){
-alert("Please select image")
+
+alert("Select image")
 return
+
 }
 
 const formData = new FormData()
-formData.append("image", file)
+
+formData.append("image",file)
+formData.append("user_id",USER_ID)
 
 const res = await fetch(API + "/dehaze",{
 
@@ -25,8 +85,32 @@ const url = window.URL.createObjectURL(blob)
 
 const a = document.createElement("a")
 
-a.href = url
-a.download = "dehazed_image.png"
+a.href=url
+a.download="dehazed.jpg"
 a.click()
+
+}
+
+
+
+async function loadHistory(){
+
+const res = await fetch(API + "/history/"+USER_ID)
+
+const images = await res.json()
+
+const div = document.getElementById("history")
+
+div.innerHTML=""
+
+images.forEach(img=>{
+
+const image=document.createElement("img")
+
+image.src=API+"/download?path="+img
+
+div.appendChild(image)
+
+})
 
 }
