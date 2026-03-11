74b5898a7db1a6035a001d69a433f7d235a47a34
afzalafzql11-max <afzalafzql11@gmail.com>
2026-03-11 19:37:49 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,121 +1,32 @@
-const API = "https://dehazer-sf8p.onrender.com"
+const API = "https://image1dehazer.onrender.com"
 
-let user_id = null
+async function uploadImage(){
 
+const file = document.getElementById("image").files[0]
 
-async function signup(){
+if(!file){
+alert("Please select image")
+return
+}
 
-let email = document.getElementById("signup_email").value
-let password = document.getElementById("signup_pass").value
+const formData = new FormData()
+formData.append("image", file)
 
-await fetch(API+"/signup",{
+const res = await fetch(API + "/dehaze",{
 
 method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body:JSON.stringify({
-email:email,
-password:password
-})
+body:formData
 
 })
 
-alert("Signup successful")
+const blob = await res.blob()
 
-}
+const url = window.URL.createObjectURL(blob)
 
+const a = document.createElement("a")
 
-async function login(){
-
-let email=document.getElementById("login_email").value
-let password=document.getElementById("login_pass").value
-
-let res=await fetch(API+"/login",{
-
-method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body:JSON.stringify({
-email:email,
-password:password
-})
-
-})
-
-let data=await res.json()
-
-if(data.status==="success"){
-
-user_id=data.user_id
-
-document.getElementById("auth").style.display="none"
-document.getElementById("dashboard").style.display="block"
-
-loadHistory()
-
-}
-
-else{
-
-alert("Invalid login")
-
-}
-
-}
-
-
-async function upload(){
-
-let file=document.getElementById("image").files[0]
-
-let form=new FormData()
-
-form.append("user_id",user_id)
-form.append("image",file)
-
-let res=await fetch(API+"/dehaze",{
-
-method:"POST",
-body:form
-
-})
-
-let blob=await res.blob()
-
-let url=URL.createObjectURL(blob)
-
-let a=document.createElement("a")
-
-a.href=url
-a.download="dehazed.jpg"
-
+a.href = url
+a.download = "dehazed_image.png"
 a.click()
-
-loadHistory()
-
-}
-
-
-async function loadHistory(){
-
-let res=await fetch(API+"/history/"+user_id)
-
-let data=await res.json()
-
-let html="<h3>History</h3>"
-
-data.forEach(img=>{
-
-html+=`<br><a href="${API}/download?path=${img}" target="_blank">Download</a>`
-
-})
-
-document.getElementById("history").innerHTML=html
 
 }
